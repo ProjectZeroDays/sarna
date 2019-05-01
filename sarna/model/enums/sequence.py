7777a82df19fa0b4bc913b6121242809673e3bc8
Raul Sampedro <rsrdesarrollo@gmail.com>
2019-05-01 20:34:38 +0200

    add sequence enum

---
+++ sarna/model/enums/sequence.py
@@ -0,0 +1,14 @@
+from sarna.model.enums.base_choice import BaseChoice
+from sarna.model.enums.language import Language
+
+
+class DocxSecuence(BaseChoice):
+    _init_ = "value translation"
+    Image = 1, {
+        Language.English: 'Image',
+        Language.Spanish: 'Imagen'
+    }
+    Table = 2, {
+        Language.English: 'Table',
+        Language.Spanish: 'Tabla'
+    }
